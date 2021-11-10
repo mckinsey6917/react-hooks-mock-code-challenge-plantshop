593773a3d031de63adc456fcd5bede209dd31dc2
mckinseyleckenby <leckenbym11@gmail.com>
2021-11-10 11:05:47 -0600

    Complete

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState }from "react";
 
-function NewPlantForm() {
-  return (
+function NewPlantForm({newPlant}) {
+  const [formData, setFormData] = useState (
+    //set this formData to be initiall an empty object so
+    //make empty object - abstracting?? 
+    //well not an empty object BUT object with the keys we want with EMPTY values
+    {
+      "id": "",
+      "name": "",
+      "image": "",
+      "price": ""
+    }
+  )
+  // {
+  //   "id": 1,
+  //   "name": "Aloe",
+  //   "image": "./images/aloe.jpg",
+  //   "price": 15.99
+  // }
+
+  //now add one function to handle all onCHange form
+  function handleOnChangeForm(e) {
+    //console.log(formData)
+    //use setFormData and create new object - spread operator here - then! the key poinst to name of target [] = differentiate 
+    //then that key points to the e.target.value (the value) 
+    setFormData({...formData, [e.target.name]:e.target.value})
+    //use spreadoperator to get copy of what is on the actual form
+    //then with e.target.name(name = name, image, price) we assign the new value to that
+}
+
+//so on input onChange we want to trigger the onChange when we add values to the object keys - update with whatever data we enter
+function handleSubmitForm (e) {
+  e.preventDefault()
+  newPlant(formData)
+}  
+
+//onSubmit has to go on ACTUAL FORM
+return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit = {handleSubmitForm}>
+        <input onChange={handleOnChangeForm} type="text" name="name" placeholder="Plant name" />
+        <input onChange={handleOnChangeForm} type="text" name="image" placeholder="Image URL" />
+        <input onChange={handleOnChangeForm} type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  //set up state locally to store plants = array of objects
+
+  const [showPlants, setShowPlants] = useState([])
+  const [filterPlants, setFilterPlants] = useState(showPlants)
+
+  function handleSearch (e) {
+    let filterPlantsList = showPlants.filter(plant => {
+      return plant.name.includes(e.target.value)
+    })
+    setFilterPlants(filterPlantsList)
+  }
+function newPlant (addPlant) {
+
+  const showNewPlant = [...showPlants, addPlant]
+}
+
+  useEffect(() => { 
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+   // .then(data => console.log(data)) to check if we are fetching the correct data
+   .then(data => {setShowPlants(data)
+   //also add filter plants to data
+   setFilterPlants(data)})
+  }, [])
+
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm newPlant ={newPlant} />
+      <Search handleSearch={handleSearch} />
+      <PlantList plants={filterPlants} />
     </main>
   );
 }
